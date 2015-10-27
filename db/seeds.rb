cf01cc0024b22bac90e708cda33a9e1fbd8591fc
Xiaomeng Yang <xiaomeng-yang@uiowa.edu>
2015-10-27 03:35:21 +0000

    initial seeding data for user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create!(name:'Maggie', email:'maggie@yang.com', state:'Iowa', profession:'Student', description:'Hi, I am Maggie!')
+User.create!(name:'Sushma', email:'sushma@sushma.com', state:'Iowa', profession:'Student', description:'Hi, I am Sushma!')
+User.create!(name:'Anu', email:'anu@anu.com', state:'Iowa', profession:'Student', description:'Hi, I am Anu!')
+User.create!(name:'Jingwei', email:'jingwei@ji.com', state:'Iowa', profession:'Student', description:'Hi, I am Jingwei. I am a boy!')
